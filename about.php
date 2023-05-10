9eaa6aad262d9b00b0d2730fd289d715e5e4a346
KarthiCF <cyberfactory1024@gmail.com>
2023-05-10 23:45:13 +0530

    adding on some extra stuffs

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container p-5">
+        <h2>A brief about HIPPO COMPUTERS</h2>
+        <p>At Hippo Computer Service, we understand the importance of technology in today's fast-paced world. Whether you're a business owner or an individual, we are here to provide you with top-notch computer services that meet your unique requirements. With a team of skilled professionals and a commitment to customer satisfaction, we offer a comprehensive range of services to keep your technology running smoothly.
+
+        Our services cover everything from computer repairs and troubleshooting to software installations, virus removal, and data recovery. Our experienced technicians have a deep understanding of various operating systems and hardware components, allowing them to diagnose and resolve issues efficiently. We utilize the latest tools and techniques to ensure that your computer receives the best possible care.
+
+        We pride ourselves on our quick turnaround times and transparent pricing. We understand the frustration that comes with computer problems, so we strive to provide timely solutions without breaking the bank. Our technicians will keep you informed throughout the process, explaining the problem and presenting suitable options for repair or replacement.
+
+        Customer satisfaction is at the heart of everything we do. We believe in building long-term relationships with our clients by providing reliable and personalized service. Whether you need assistance with a single computer or require ongoing IT support for your business, Hippo Computer Service is here to help.
+
+        Contact us today to experience the Hippo difference. Our friendly staff will be happy to answer your questions and schedule an appointment that fits your schedule. Trust Hippo Computer Service for all your technology needs – we'll have your devices up and running smoothly in no time!
+
+</p>
+    </div>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hippo Computer Services</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
@@ -23,10 +25,10 @@
           <a class="nav-link" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+          <a class="nav-link" href="services.php">Services</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="about.php">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact</a>
@@ -35,10 +37,118 @@
     </div>
   </nav>
 
+  <section class="main ">
   <!-- Page content -->
-  <div class="container mt-4">
+  <div class="container-fluid mt-4" >
+    <div class="index_title p-5" style="background-color: steelblue ">
     <h1>Welcome to Hippo Computer Services</h1>
     <p>Offering professional computer repair and IT services.</p>
+    </div>
+
+
+    <div class="flex-container">
+      <div class="flex-item">
+        <div class="content">
+          <h3>Data Recovery</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Data Recovery goes here.</p>
+        </div>
+      </div>
+      <div class="flex-item">
+        <div class="content">
+          <h3>Hard Disk</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Hard Disk goes here.</p>
+        </div>
+      </div>
+      <div class="flex-item">
+        <div class="content">
+          <h3>Sales</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Sales goes here.</p>
+        </div>
+      </div>
+      <div class="flex-item">
+        <div class="content">
+          <h3>Computers</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Computers goes here.</p>
+        </div>
+      </div>
+      <div class="flex-item">
+        <div class="content">
+          <h3>Printers</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Printers goes here.</p>
+        </div>
+      </div>
+      <div class="flex-item">
+        <div class="content">
+          <h3>Laptops</h3>
+        </div>
+        <div class="hover-content">
+          <p>Content about Laptops goes here.</p>
+        </div>    
+      </div>
+</div>
+</section>
+
+  <footer class="site-footer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">At Hippo Computer Service, we understand the importance of technology in today's fast-paced world. Whether you're a business owner or an individual, we are here to provide you with top-notch computer services that meet your unique requirements. Our services cover everything from computer repairs and troubleshooting to software installations, virus removal, and data recovery. Our experienced technicians have a deep understanding of various operating systems and hardware components, allowing them to diagnose and resolve issues efficiently. We utilize the latest tools and techniques to ensure that your computer receives the best possible care.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li class="footer-sites"><a href="#">Data recovery</a></li>
+              <li class="footer-sites"><a href="#">OS services</a></li>
+              <li class="footer-sites"><a href="#">Software installation</a></li>
+              <li class="footer-sites"><a href="#">Virus removal</a></li>
+          
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Contribute</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by 
+         <a href="#">HIPPO COMPUTERS</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons ">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
   
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
